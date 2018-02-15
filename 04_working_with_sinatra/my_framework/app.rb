39f9e3b7af38f994b535bc755eb57e4d99571b7c
PetePhx <pete0phx@yandex.com>
2018-02-15 12:33:57 -0700

    separate framework class

--- 04_working_with_sinatra/my_framework/app.rb
+++ 04_working_with_sinatra/my_framework/app.rb
@@ -1,41 +1,22 @@
+require_relative 'monroe'
 require_relative 'advice'
 
-class App
+class App < Monroe
   def call(env)
     case env['REQUEST_PATH']
     when '/'
       status = '200'
-      headers = {"Content-Type" => 'text/html'}
-      response(status, headers) do
-        erb :index
-      end
+      headers = { "Content-Type" => 'text/html' }
+      response(status, headers) { erb :index }
     when '/advice'
       piece_of_advice = Advice.new.generate
       status = '200'
-      headers = {"Content-Type" => 'text/html'}
-      response(status, headers) do
-        erb :advice, message: piece_of_advice
-      end
+      headers = { "Content-Type" => 'text/html' }
+      response(status, headers) { erb :advice, message: piece_of_advice }
     else
       status = '404'
-      headers = {"Content-Type" => 'text/html', "Content-Length" => '61'}
-      response(status, headers) do
-        erb :not_found
-      end
+      headers = { "Content-Type" => 'text/html', "Content-Length" => '61' }
+      response(status, headers) { erb :not_found }
     end
-  end
-
-  private
-
-  def erb(filename, local = {})
-    b = binding
-    message = local[:message]
-    content = File.read("views/#{filename}.erb")
-    ERB.new(content).result(b)
-  end
-
-  def response(status, headers, body = '')
-    body = yield if block_given?
-    [status, headers, [body]]
   end
 end
