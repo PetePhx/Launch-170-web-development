f2b6b5d8f126dbdcc3e3d0071dd400b62c3cac97
PetePhx <pete0phx@yandex.com>
2018-02-19 12:09:41 -0700

    book-viewer: helpers for paragraphs

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -2,14 +2,22 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+helpers do
+  def in_pargraphs(str)
+    str.split("\n\n").map { |e| "<p>#{e}</p>" }.join("\n")
+  end
+end
+
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
   erb :home
 end
 
 get "/chapters/:number" do
-  @contents = File.readlines("data/toc.txt")
   @number = params['number'].to_i
   @title = "Chapter #{@number}: #{@contents[@number - 1]}"
   @chapter = File.read("data/chp#{@number}.txt")
