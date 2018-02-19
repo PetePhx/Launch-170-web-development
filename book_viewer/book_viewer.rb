7596997a9f7e30de4ef61fef908cb041f77f0d44
PetePhx <pete0phx@yandex.com>
2018-02-19 11:23:47 -0700

    book-viewer: routing chapters

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -8,9 +8,10 @@ get "/" do
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
+get "/chapters/:number" do
   @contents = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp1.txt")
+  @number = params['number'].to_i
+  @title = "Chapter #{@number}: #{@contents[@number - 1]}"
+  @chapter = File.read("data/chp#{@number}.txt")
   erb :chapter
 end
