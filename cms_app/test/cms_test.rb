650138b0990ecef764297383911f206987a69678
PetePhx <pete0phx@yandex.com>
2018-03-06 10:06:45 -0700

    test for redirect from nonexistent url

--- cms_app/test/cms_test.rb
+++ cms_app/test/cms_test.rb
@@ -21,24 +21,31 @@ class AppTest < Minitest::Test
     assert_includes last_response.body, "history.txt"
   end
 
-  def test_about
+  def test_about_changes_history
     get "/about.txt"
     assert_equal 200, last_response.status
     assert_equal "text/plain;charset=utf-8", last_response["Content-Type"]
     assert_includes last_response.body, "semiaquatic egg-laying mammal"
-  end
 
-  def test_changes
     get "/changes.txt"
     assert_equal 200, last_response.status
     assert_equal "text/plain;charset=utf-8", last_response["Content-Type"]
     assert_includes last_response.body, "the Quaternary period"
-  end
 
-  def test_history
     get "/history.txt"
     assert_equal 200, last_response.status
     assert_equal "text/plain;charset=utf-8", last_response["Content-Type"]
     assert_includes last_response.body, "Matsumoto dreams"
+  end
+
+  def test_document_not_found
+  get "/virus.exe"
+
+  assert_equal 302, last_response.status
+
+  get last_response["Location"]
+
+  assert_equal 200, last_response.status
+  assert_includes last_response.body, "virus.exe does not exist."
   end
 end
