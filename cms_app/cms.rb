4e6f2f67b37e1f5b09eff4b5b77d41514d1b7450
PetePhx <pete0phx@yandex.com>
2018-03-06 10:00:57 -0700

    error message for non-existent files.

--- cms_app/cms.rb
+++ cms_app/cms.rb
@@ -3,6 +3,9 @@ require "sinatra/reloader" if development?
 require "sinatra/content_for"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+end
 
 before do
   @root = "/home/user/Launch/170_web_development/cms_app"
@@ -16,11 +19,14 @@ get "/" do
 end
 
 get "/:file" do |file|
-  redirect not_found, 301 unless @files.include? file
+  unless @files.include? file
+    session[:error] = "#{file} does not exist."
+    redirect "/"
+  end
   headers["Content-Type"] = "text/plain;charset=utf-8"
   File.read("#{@root}/data/#{file}")
 end
 
-not_found do
-  "BAD URL."
-end
+# not_found do
+#   "BAD URL."
+# end
